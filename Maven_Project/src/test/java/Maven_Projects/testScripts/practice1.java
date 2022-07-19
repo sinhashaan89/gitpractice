2f4e7979a6416384bc27246eda5ea42f244c4c5f
Sayantan Sinha <sinhashaan89@gmail.com>
2022-07-19 11:44:06 +0530

    Commited

---
+++ Maven_Project/src/test/java/Maven_Projects/testScripts/practice1.java
@@ -0,0 +1,18 @@
+package Maven_Projects.testScripts;
+
+import org.testng.annotations.Test;
+
+public class practice1 {
+@Test
+public void Practice1(){
+	System.out.println("This is method of maven for practice1,Practice1");
+}
+@Test
+public void Practice2(){
+	System.out.println("This is method of maven for practice1,Practice2");
+}
+@Test
+public void Practice3(){
+	System.out.println("This is method of maven for practice1,Practice3");
+}
+}
---
+++ Maven_Project/src/test/java/Maven_Projects/testScripts/practice2.java
@@ -0,0 +1,22 @@
+package Maven_Projects.testScripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class practice2 {
+	@Test
+	public void Practice10(){
+		String url = System.getProperty("URL");
+		System.out.println("This is my browser"+url);
+		Reporter.log("This is method of maven for practice2,Practice10",true);
+	}
+	@Test
+	public void Practice20(){
+		Reporter.log("This is method of maven for practice2,Practice20",true);
+	}
+	@Test
+	public void Practice30(){
+		Reporter.log("This is method of maven for practice2,Practice30",true);
+	}
+
+}
