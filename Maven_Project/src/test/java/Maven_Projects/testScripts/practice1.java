c1c521df2f64c8fc2a72ca856daee7f390f6a433
sinhashaan89 <sinhashaan89@gmail.com>
2022-07-19 12:50:21 +0530

    Update practice1.java

--- Maven_Project/src/test/java/Maven_Projects/testScripts/practice1.java
+++ Maven_Project/src/test/java/Maven_Projects/testScripts/practice1.java
@@ -15,4 +15,9 @@ public void Practice2(){
 public void Practice3(){
 	System.out.println("This is method of maven for practice1,Practice3");
 }
+@Test
+public void Practice3(){
+	System.out.println("This is method of maven for practice1,Practice3");
+}
+	
 }
