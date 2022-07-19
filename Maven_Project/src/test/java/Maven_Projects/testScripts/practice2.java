a18181378849ea85f2393532045ba5f206809791
Sayantan Sinha <sinhashaan89@gmail.com>
2022-07-19 12:08:57 +0530

    method added

--- Maven_Project/src/test/java/Maven_Projects/testScripts/practice2.java
+++ Maven_Project/src/test/java/Maven_Projects/testScripts/practice2.java
@@ -14,6 +14,9 @@ public class practice2 {
 	public void Practice20(){
 		Reporter.log("This is method of maven for practice2,Practice20",true);
 	}
+	public void Practice40() {
+		Reporter.log("This is method of maven for practice2,Practice40",true);
+	}
 	@Test
 	public void Practice30(){
 		Reporter.log("This is method of maven for practice2,Practice30",true);
