f279da6bdd10a3d7631a92022d7d4b06ab8839d2
Sayantan Sinha <sinhashaan89@gmail.com>
2022-07-20 13:03:49 +0530

    commit3

--- Maven_Project/src/test/java/Maven_Projects/testScripts/practice2.java
+++ Maven_Project/src/test/java/Maven_Projects/testScripts/practice2.java
@@ -14,7 +14,7 @@ public class practice2 {
 	public void Practice20(){
 		Reporter.log("This is method of maven for practice2,Practice20",true);
 	}
-	public void Practice4009() {
+	public void Practice434() {
 		Reporter.log("This is method of maven for practice2,Practice40",true);
 	}
 	@Test
